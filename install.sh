53ffb9c947eeb5b01adc84554971fe1e360ac3fc
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-05-29 18:12:43 +0300

    Update install.sh
    added some repos using the tutorial here as a guide: https://fedoramagazine.org/how-i-customize-fedora-silverblue-and-fedora-kinoite/

--- install.sh
+++ install.sh
@@ -3,18 +3,30 @@
 ###################################
 
 # Some variables 
+install="rpm-ostree install"
+remove=rpm-"ostree uninstall"
 apps_list=https://dwdeath.github.io/FavApps_FK/list.txt
 
 
 # Update the system
 rpm-ostree upgrade
+rpm-ostree update
 
 # Remove Packages I don't need 
-# firefox
-# vlc
+$remove firefox
+$remove vlc
 
 # Enable some stuff 
+flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kdeapps.flatpakrepo
+flatpak update --appstream
+sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo rpm-ostree install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# Enable copr packages
+sudo ostree remote add rtl88x2bu https://download.copr.fedorainfracloud.org/results/farribeiro/rtl88x2bu-kmod/fedora-$releasever-$basearch/
+#sudo ostree remote add 
 
 # Install some packages
-rpm-ostree $(awk '{print $1}' curl -s apps_list)
+sudo $install $(awk '{print $1}' curl -s apps_list)
