4a0bfc9acf59d72c4c9772170e0371417ba6aa34
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-05-29 18:54:58 +0300

    Update install.sh
    added variables

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@
 install="rpm-ostree install "
 remove="rpm-ostree uninstall "
 hard_remove="rpm-ostree override remove "
-copr_repo="sudo wget -P /etc/yum.repos.d/ "
+add_copr_repo="sudo wget -P /etc/yum.repos.d/ "
 apps_list=https://dwdeath.github.io/FavApps_FK/list.txt
+repo_list_copr="https://dwdeath.github.io/FavApps_FK/copr_repo.txt"
 
 
 # Update the system
@@ -27,9 +28,10 @@ sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free
 sudo rpm-ostree install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Enable copr packages
-$copr_repo https://copr.fedorainfracloud.org/coprs/benjaminfo/rtl8822bu/repo/fedora-36/benjaminfo-rtl8822bu-fedora-36.repo
-$copr_repo https://copr.fedorainfracloud.org/coprs/farribeiro/rtl88x2bu-kmod/repo/fedora-36/farribeiro-rtl88x2bu-kmod-fedora-36.repo
-$copr_repo https://copr.fedorainfracloud.org/coprs/scorreia/rtl88x2bu/repo/fedora-36/scorreia-rtl88x2bu-fedora-36.repo
+$add_copr_repo $(awk '{print $1}' curl -s repo_list_copr)
+# $copr_repo https://copr.fedorainfracloud.org/coprs/benjaminfo/rtl8822bu/repo/fedora-36/benjaminfo-rtl8822bu-fedora-36.repo
+# $copr_repo https://copr.fedorainfracloud.org/coprs/farribeiro/rtl88x2bu-kmod/repo/fedora-36/farribeiro-rtl88x2bu-kmod-fedora-36.repo
+# $copr_repo https://copr.fedorainfracloud.org/coprs/scorreia/rtl88x2bu/repo/fedora-36/scorreia-rtl88x2bu-fedora-36.repo
 #sudo ostree remote add rtl88x2bu https://download.copr.fedorainfracloud.org/results/benjaminfo/rtl8822bu/fedora-$releasever-$basearch/
 #sudo ostree remote add 
 
