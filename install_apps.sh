82a43759f52eb3f98b35156bde598bebad83a3e4
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-05-29 20:35:56 +0300

    Create install_apps.sh
    initialized script

---
+++ install_apps.sh
@@ -0,0 +1,22 @@
+###################################
+## Eddie's Custom Fedora Kinoite ##
+###################################
+
+# Some variables 
+install="rpm-ostree install "
+remove="rpm-ostree uninstall "
+hard_remove="rpm-ostree override remove "
+add_copr_repo="sudo wget -P /etc/yum.repos.d/ "
+apps_list=https://dwdeath.github.io/FavApps_FK/list.txt
+repo_list_copr="https://dwdeath.github.io/FavApps_FK/copr_repo.txt"
+
+# Remove Packages I don't need 
+$remove firefox
+#$remove vlc
+
+# Install some packages
+$install $(awk '{print $1}' curl -s apps_list)
+exit 0
+
+# Reboot
+systemctl reboot
